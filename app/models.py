68b045547cccf3aceb9b4547dce56375c7ad0b8f
Suhen M <suhen.mg02@gmail.com>
2025-08-23 14:21:38 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class SignUp(models.Model):
+    username=models.CharField(max_length=30)
+    email=models.EmailField()
+    password=models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.username
+
+
+
 # Create your models here.
