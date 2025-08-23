d11edf45e48aef2f0e0512896ee13d4c97fb5e6f
Suhen M <suhen.mg02@gmail.com>
2025-08-23 14:17:27 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from app import views
 
 urlpatterns=[
-    path('',view=views.home,name='home'),
+    path('home/',view=views.home,name='home'),
+    path('analyze/',views.analyze,name='analyze'),
+    path('',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
 ]
